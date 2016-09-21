50238649b63e74cdaffb34ceca9687291b303a38
S4kur4 <2570160655@qq.ccom>
2016-09-21 21:28:06 +0800

    add caesarDecrypt.py
    a tool to decrypt caesar password.

---
+++ caesarDecrypt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#This is a tool to decrypt caesar password, it can list all possible cases.
+import optparse
+import sys
+
+payload = 'abcdefghijklmnopqrstuvwxyz'
+
+def main():
+	parser = optparse.OptionParser('usage: %prog [options]')
+	parser.add_option('-p', '--password', dest='password', type='string', help='specify a caesar password to decrypt')
+	(options, args) = parser.parse_args()
+	if (options.password == None):
+		parser.print_help()
+		sys.exit(0)
+	else:
+		password = options.password
+	for i in range(26):
+		str1 = ''
+		for word in password:
+			if 96 < ord(word) < 123:
+				str1 = str1 + payload[((ord(word)+i)-97)%26]
+			else:
+				str1 = str1 + word
+		print "[offset %d]: %s" %(i, str1)
+
+if __name__ == '__main__':
+	main()
