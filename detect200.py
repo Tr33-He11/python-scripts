ae7d51c3340ef266cdc7598f7c6882b12e7f8dc6
S4kur4 <2570160655@qq.ccom>
2017-05-08 16:28:04 +0800

    Adding some changes
    Adding some changes.

--- detect200.py
+++ detect200.py
@@ -8,7 +8,9 @@ import threading
 import Queue
 import urllib2
 
-user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"}
+connectable_num = 0
+unconnectable_num = 0
 
 def build_weblist(weblist_file):
     fd = open(weblist_file, "rb")
@@ -21,26 +23,34 @@ def build_weblist(weblist_file):
     return words
 
 def scanner(word_queue):
-    while not word_queue.empty(): 
+    global connectable_num
+    global unconnectable_num
+    while not word_queue.empty():
         attempt = word_queue.get()
         attempt_list = []
         attempt_list.append("%s" % attempt)
         for scanner in attempt_list:
             url = scanner
             try:
-                headers = {}
-                headers["User-Agent"] = user_agent
                 r = urllib2.Request(url, headers = headers)
-                response = urllib2.urlopen(r, data=None, timeout=10)
-                print "[+]%d => %s" % (response.code, url)
+                response = urllib2.urlopen(r, data=None, timeout=7)
+                print "\033[1;32m[+] Connectable.Status code %d => %s\033[0m" % (response.code, url)
+                connectable_num += 1
             except Exception,e:
-                print "[Unconnected] => %s" % url
+                print "\033[1;31m[-] Unconnectable. => %s\033[0m" % url
+                unconnectable_num += 1
 
 def main():
-	word_queue = build_weblist("/../weblist.txt") #here select your weblist.txt
-	for i in range(30):
-		t = threading.Thread(target = scanner, args = (word_queue, ))
-		t.start()
+    tsk = []
+    word_queue = build_weblist("./weblist.txt") #here select your weblist.txt
+    for i in range(7):
+        t = threading.Thread(target = scanner, args = (word_queue,))
+        t.start()
+        tsk.append(t)
+    for t in tsk:
+        t.join()
+    print "\033[1;34m[*] %d sites are Connectable.\033[0m" % connectable_num
+    print "\033[1;34m[*] %d sites are Unconnectable.\033[0m" % unconnectable_num
 
 if __name__ == '__main__':
-	main()
+    main()
